d0a2b0c281a90720ff8e1373eda3ea15fd09f251
ccmm <mahfudzidris@bnm.gov.my>
2024-01-16 17:11:13 +0800

    update

--- resources/views/vendor/backpack/base/inc/head.blade.php
+++ resources/views/vendor/backpack/base/inc/head.blade.php
@@ -1,23 +1,33 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    @if (config('backpack.base.meta_robots_content'))<meta name="robots" content="{{ config('backpack.base.meta_robots_content', 'noindex, nofollow') }}"> @endif
+    @if (config('backpack.base.meta_robots_content'))
+        <meta name="robots" content="{{ config('backpack.base.meta_robots_content', 'noindex, nofollow') }}">
+    @endif
 
     <meta name="csrf-token" content="{{ csrf_token() }}" /> {{-- Encrypted CSRF token for Laravel, in order for Ajax requests to work --}}
-    <title>{{ isset($title) ? $title.' :: '.config('backpack.base.project_name') : config('backpack.base.project_name') }}</title>
+    <title>
+        {{ isset($title) ? $title . ' :: ' . config('backpack.base.project_name') : config('backpack.base.project_name') }}
+    </title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     @yield('before_styles')
     @stack('before_styles')
-    
+
     @if (config('backpack.base.styles') && count(config('backpack.base.styles')))
         @foreach (config('backpack.base.styles') as $path)
-        <link rel="stylesheet" type="text/css" href="{{ asset($path).'?v='.config('backpack.base.cachebusting_string') }}">
+            <link rel="stylesheet" type="text/css"
+                href="{{ asset($path) . '?v=' . config('backpack.base.cachebusting_string') }}">
         @endforeach
     @endif
 
     @if (config('backpack.base.mix_styles') && count(config('backpack.base.mix_styles')))
         @foreach (config('backpack.base.mix_styles') as $path => $manifest)
-        <link rel="stylesheet" type="text/css" href="{{ mix($path, $manifest) }}">
+            <link rel="stylesheet" type="text/css" href="{{ mix($path, $manifest) }}">
         @endforeach
     @endif
 
