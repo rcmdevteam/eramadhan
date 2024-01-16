ef3dbea35f8f9f00e7d2a4c3fd03122cf22b42c6
ccmm <mahfudzidris@bnm.gov.my>
2024-01-16 17:14:21 +0800

    update

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Selamat Datan ke eRmadhan</title>
+    <title>Selamat Datang ke eRmadhan</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
