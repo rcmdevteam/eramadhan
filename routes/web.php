30d791df8768608f98e7884ecec339425232f87c
ccmm <mahfudzidris@bnm.gov.my>
2024-02-09 18:13:38 +0800

    udpate

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes!
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
